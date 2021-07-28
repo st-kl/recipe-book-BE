3cbbc84d8f7dcff211671d52d569fad45c370512
two-wheels01 <mail.name@mailbox.org>
2021-07-28 13:00:55 +0100

    udpate model

--- BE/controllers/recipes.controllers.js
+++ BE/controllers/recipes.controllers.js
@@ -1,5 +1,5 @@
 const { selectRecipes } = require('../models/recipes.models');
 
-exports.getRecipes = (req, res, next) => {
+exports.getRecipes = (req, res) => {
   selectRecipes().then((recipes) => res.status(200).send({ recipes }));
 };
